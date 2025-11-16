2a2db4b46c504cf871861fe7c32d50cfc110af12
Lavinia Rus <lavinia.rus@student.upt.ro>
2025-11-16 22:27:29 +0200

    create file for database connection (that will be later added to main)

---
+++ backend/database/mysql.go
@@ -0,0 +1,18 @@
+package database
+import (
+	"log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "root:admin@tcp(127.0.0.1:3306)/urlshortener?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Could not connect to MySQL:", err)
+	}
+	DB = db
+	log.Println("Connected to MySQL")
+}
