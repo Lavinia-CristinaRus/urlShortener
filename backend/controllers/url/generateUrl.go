12d54ec28e6d8cfa35930df6c5f1f200ea3f9b39
Lavinia Rus <lavinia.rus@student.upt.ro>
2025-12-08 02:49:45 +0200

    removed default expiration date and moved url customization to another module

--- backend/controllers/url/generateUrl.go
+++ backend/controllers/url/generateUrl.go
@@ -60,7 +60,6 @@ func GenerateUrl(c *gin.Context) {
 		Long_url:    body.Url,
 		Short_url:   Short_url,
 		Created_at:  time.Now(),
-		Expires_at:  time.Now().AddDate(0, 6, 0),
 		Iduser:      body.Iduser,
 	}
 
@@ -70,37 +69,4 @@ func GenerateUrl(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{"short_url": Short_url})
-}
-
-func CustomizeUrl(c *gin.Context) {
-	var body struct {
-		Idurl        uint `json:"Idurl"`
-		Customurl	 string `json:"Customurl"`
-	}
-
-	if err := c.BindJSON(&body); err != nil {
-		c.JSON(400, gin.H{"error": "Invalid input"})
-		return
-	}
-
-	var found models.Url
-	err := database.DB.Where("short_url = ?", body.Customurl).First(&found).Error
-	if err != nil {
-		if err == gorm.ErrRecordNotFound {
-			if err := database.DB.Model(&models.Url{}).Where("idurl = ?", body.Idurl).Update("short_url", body.Customurl).Error; err != nil {
-				c.JSON(400, gin.H{"error": "Failed to update the url"})
-				log.Println("Update failed:", err)
-				return
-			}
-			c.JSON(200, gin.H{"message": "Url successfully customized"})
-			return
-		}
-		c.JSON(400, gin.H{"error": "Failed to update the url"})
-		log.Println("DB error while checking short URL:", err)
-		return
-	}
-
-	c.JSON(400, gin.H{"error": "Short url provided is already in use"})
-	log.Println("Url already in use")
-	return
 }
---
+++ backend/controllers/url/modifyUrl.go
@@ -0,0 +1,43 @@
+package url
+
+import (
+	"time"
+	"gorm.io/gorm"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"url-shortener/database"
+	"url-shortener/models"
+)
+func CustomizeUrl(c *gin.Context) {
+	var body struct {
+		Idurl        uint `json:"Idurl"`
+		Customurl	 string `json:"Customurl"`
+	}
+
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	var found models.Url
+	err := database.DB.Where("short_url = ?", body.Customurl).First(&found).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			if err := database.DB.Model(&models.Url{}).Where("idurl = ?", body.Idurl).Update("short_url", body.Customurl).Error; err != nil {
+				c.JSON(400, gin.H{"error": "Failed to update the url"})
+				log.Println("Update failed:", err)
+				return
+			}
+			c.JSON(200, gin.H{"message": "Url successfully customized"})
+			return
+		}
+		c.JSON(400, gin.H{"error": "Failed to update the url"})
+		log.Println("DB error while checking short URL:", err)
+		return
+	}
+
+	c.JSON(400, gin.H{"error": "Short url provided is already in use"})
+	log.Println("Url already in use")
+	return
+}
